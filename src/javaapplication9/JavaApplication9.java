32deb010a4f9c3d658a3c74bc5af60b6cfe7ddfb
Alumno <Alumno@a3profe>
2019-01-30 17:43:02 +0100

    segundo proyecto

--- src/javaapplication9/JavaApplication9.java
+++ src/javaapplication9/JavaApplication9.java
@@ -15,7 +15,7 @@ public class JavaApplication9 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hola");
+        System.out.println("holayghfgh");
     }
     
 }
